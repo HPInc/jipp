aa2a38b98008c4019de5ed6c4d42c6dd76b4eea1
Glade Diviney <glade.diviney@hp.com>
2017-04-25 15:09:59 -0700

    Clean up some check errors

--- src/test/java/com/hp/jipp/client/IppClientTest.java
+++ src/test/java/com/hp/jipp/client/IppClientTest.java
@@ -2,11 +2,8 @@ package com.hp.jipp.client;
 
 
 import org.junit.Test;
-import org.junit.Rule;
-import org.junit.rules.ExpectedException;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 
@@ -15,25 +12,13 @@ import static org.junit.Assert.*;
 import com.hp.jipp.encoding.AttributeGroup;
 import com.hp.jipp.encoding.Packet;
 import com.hp.jipp.encoding.Tag;
+import com.hp.jipp.model.Operation;
 import com.hp.jipp.model.Status;
 import com.hp.jipp.model.Attributes;
 
 public class IppClientTest {
     URI printerUri = new URI("ipp://sample.com");
-    URI jobUri = new URI("ipp://sample.com/jobs/0001");
     IppPrinter printer = IppPrinter.of(printerUri);
-    BaseDocument document = new BaseDocument() {
-        @Override
-        public String getDocumentType() {
-            return "application/txt";
-        }
-
-        @Override
-        public InputStream openDocument() throws IOException {
-            return null;
-        }
-    };
-    JobRequest jobRequest = JobRequest.of(printer, document);
     FakeTransport transport = new FakeTransport();
 
     IppClient client = new IppClient(transport);
@@ -61,6 +46,8 @@ public class IppClientTest {
         responsePacket = Packet.create(Status.Ok, 0x01, AttributeGroup.create(Tag.PrinterAttributes,
                 Attributes.PrinterInfo.of("printername")));
         printer = client.getPrinterAttributes(printer);
+        assertEquals(Operation.GetPrinterAttributes, sendPacket.getCode(Operation.ENCODER));
+        assertEquals(printer.getUri(), sendUri);
         assertEquals("printername", printer.getAttributes().getValue(Attributes.PrinterInfo).get());
     }
 
