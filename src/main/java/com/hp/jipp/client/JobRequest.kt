b3b28cef8a949ea2fddec4abb27c2424268bd354
Glade Diviney <glade.diviney@hp.com>
2017-08-14 13:38:29 -0700

    removing jipp-client files

--- src/main/java/com/hp/jipp/client/JobRequest.kt
+++
@@ -1,3 +0,0 @@
-package com.hp.jipp.client
-
-data class JobRequest(val printer: Printer, val name: String, val document: Document)
--- src/main/java/com/hp/jipp/client/ValidatedJob.kt
+++
@@ -1,5 +0,0 @@
-package com.hp.jipp.client
-
-import com.hp.jipp.model.Packet
-
-data class ValidatedJob(val jobRequest: JobRequest, val packet: Packet)
