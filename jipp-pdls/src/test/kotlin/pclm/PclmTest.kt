9c503a29e72e4c54883fde0ee5103813de048aef
Glade Diviney <glade.diviney@hp.com>
2018-04-25 13:42:47 -0700

    Include code coverage check in jipp-pdls

--- jipp-pdls/src/test/kotlin/pclm/PclmTest.kt
+++ jipp-pdls/src/test/kotlin/pclm/PclmTest.kt
@@ -1,10 +1,12 @@
 package pclm
 
-import util.ByteWindow
+import java.io.ByteArrayOutputStream
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import com.hp.jipp.doc.RenderablePage
 import com.hp.jipp.doc.pclm.PclmCapabilities
 import com.hp.jipp.doc.pclm.PclmWriter
-import java.io.ByteArrayOutputStream
-import org.junit.Test
+import util.ByteWindow
 import util.RandomDocument
 
 class PclmTest {
@@ -19,9 +21,14 @@ class PclmTest {
         cyclePclm(PclmCapabilities(32, false))
     }
 
-    private fun cyclePclm(caps: PclmCapabilities) {
+    @Test
+    fun pixelsToPoints() {
+        assertEquals(72.0, RenderablePage.pixelsToPoints(300, RenderablePage.pointsToPixels(300, 72.0)), 0.0001)
+    }
 
-        val randomDocument = RandomDocument(12345L, 2, 72.0, 72.0)
+    private fun cyclePclm(caps: PclmCapabilities) {
+        // Use a tall enough page so that we're assured there will be at least one blank area
+        val randomDocument = RandomDocument(12345L, 2, 72.0, 150.0)
         val bytesOut = ByteArrayOutputStream()
         PclmWriter(bytesOut, caps, 300).use {
             it.write(randomDocument)
