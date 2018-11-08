7f0cd6b3a15247787231aa9b53798016abacbadf
Glade Diviney <glade.diviney@hp.com>
2018-11-08 11:41:31 -0800

    Add keyword | name type

---
+++ jipp-core/src/main/java/com/hp/jipp/encoding/KeywordOrName.kt
@@ -0,0 +1,34 @@
+// Copyright 2017 HP Development Company, L.P.
+// SPDX-License-Identifier: MIT
+
+package com.hp.jipp.encoding
+
+import java.lang.IllegalArgumentException
+
+/**
+ * Describes an object which may contain either a name [Name] or a keyword [String].
+ *
+ * See [RFC8011 Section 5.1.3](https://tools.ietf.org/html/rfc8011#section-5.1.3).
+ */
+data class KeywordOrName constructor(val name: Name?, val keyword: String?) : TaggedValue(), Stringable {
+
+    /** Construct a [KeywordOrName] containing only a keyword. */
+    constructor(keyword: String) : this(null, keyword)
+
+    /** Construct a [KeywordOrName] containing only a name. */
+    constructor(name: Name) : this(name, null)
+
+    init {
+        name ?: keyword ?: throw IllegalArgumentException("both .name and .keyword are null")
+        if (name != null && keyword != null) throw IllegalArgumentException("both .name and .keyword are present")
+    }
+
+    override val tag: Tag = name?.tag ?: Tag.keyword
+
+    override val value = name ?: keyword!!
+
+    override fun asString() = name?.asString() ?: keyword!!
+
+    override fun toString() =
+        name?.toString() ?: keyword!!
+}
